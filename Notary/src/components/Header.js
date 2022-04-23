c37780deb9fab5a63b0541c7c0db156341fb45a7
Shuja Ali <shujaali1234@gmail.com>
2022-04-23 07:43:19 +0500

    Changes

--- Notary/src/components/Header.js
+++ Notary/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>Notary</Navbar.Brand>
+            <Navbar.Brand>Notary.Ink</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
